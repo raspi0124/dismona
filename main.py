18a99d1229ffef2a491b88bbb75d034b9592f66a
raspi0124 <adores0124@yahoo.co.jp>
2018-02-06 05:56:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ async def on_ready():
 
 INDB = "aaaa"
 
+message_author = message.author
 @client.event
 async def on_message(message):
     # 「/register」で始まるか調べる
@@ -35,7 +36,7 @@ async def on_message(message):
                 await client.send_message(message.channel, m)
             # subprocess.check_output(["monacoin-cli getaddressesbyaccount" + message.author.name + ])
                 print ('Creating ' + message.author.name + "'s account..")
-                cursor.execute("INSERT INTO id(id, name) VALUES ('message.author', address)")
+                cursor.execute("INSERT INTO id(id, name) VALUES ('message_author', address)")
                 m = "Created your account succefully! your address is <address>enjoy!"
                 await client.send_message(message.channel, m)
 
