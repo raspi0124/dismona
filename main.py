926fbb9881941fa9198242b645bacebc31c4bfd0
raspi0124 <adores0124@yahoo.co.jp>
2018-02-17 18:00:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,49 +35,48 @@ async def on_message(message):
         # 送り主がBotだった場合反応したくないので
         if client.user != message.author.name:
             # メッセージを書きます
-            if message.author.name != INDB:
-                m = "<@" + message.author.id + "> さんのアカウントを作成しますね！"
+            m = "<@" + message.author.id + "> さんのアカウントを作成しますね！"
             # メッセージが送られてきたチャンネルへメッセージを送ります
-                await client.send_message(message.channel, m)
-                cmd = "monacoin-cli getnewaddress " + message.author.id + ""
-                rut  =  subprocess.check_output( cmd.split(" ") )
-                print ('Creating <' + message.author.id + ">'s account.. user ID ")
-                print ("---1---")
-                #cursor.execute("insert into dismona.id(id,address) values('message_author', address);")
-                resultaddress = rut.decode()
-                resultmore = resultaddress.replace('[', '')
-                resultmore2 = resultmore.replace(']', '')
-                resultmore3 = resultmore2.replace('"', '')
-                resultmore4 = resultmore3.replace("\n", "")
-                resultmore5 = resultmore4.replace(" ", "")
-                print ("---2---")
-                #DEBUG
-                print ("---decoded---")
-                print (resultaddress)
-                print ("-----------------")
-                print ("---address---")
-                print(resultaddress)
-                print ("----------------")
-                print ("---resultmoreaddress---")
-                print (resultmore3)
-                print ("------------------------------")
-                print ("---removednaddress---")
-                print (resultmore4)
-                print("-------------------------------")
-                print ("---removedsaddress---")
-                print (resultmore5)
-                print("-------------------------------")
-                #DEBUG FIN
-                print ("---3---")
-                cursor.execute("INSERT INTO dismona.id(id, address) VALUES ('" + message.author.id + "', '" + resultmore5 + "' )")
-                print ("---4---")
-                print ('----MYSQL COMMAND START----')
-                print ("INSERT INTO dismona.id(id, address) VALUES ('" + message.author.id + "', '" + resultmore5 + "' )")
-                print ('----MYSQL COMMAND END----')
-                print ("---5---")
-                m = "<@"+ message.author.id + "> ,Created your account succefully! your address is " + resultmore5 + " enjoy!"
-                print ("---6---")
-                await client.send_message(message.channel, m)
+            await client.send_message(message.channel, m)
+            cmd = "monacoin-cli getnewaddress " + message.author.id + ""
+            rut  =  subprocess.check_output( cmd.split(" ") )
+            print ('Creating <' + message.author.id + ">'s account.. user ID ")
+            print ("---1---")
+            #cursor.execute("insert into dismona.id(id,address) values('message_author', address);")
+            resultaddress = rut.decode()
+            resultmore = resultaddress.replace('[', '')
+            resultmore2 = resultmore.replace(']', '')
+            resultmore3 = resultmore2.replace('"', '')
+            resultmore4 = resultmore3.replace("\n", "")
+            resultmore5 = resultmore4.replace(" ", "")
+            print ("---2---")
+            #DEBUG
+            print ("---decoded---")
+            print (resultaddress)
+            print ("-----------------")
+            print ("---address---")
+            print(resultaddress)
+            print ("----------------")
+            print ("---resultmoreaddress---")
+            print (resultmore3)
+            print ("------------------------------")
+            print ("---removednaddress---")
+            print (resultmore4)
+            print("-------------------------------")
+            print ("---removedsaddress---")
+            print (resultmore5)
+            print("-------------------------------")
+            #DEBUG FIN
+            print ("---3---")
+            cursor.execute("INSERT INTO dismona.id(id, address) VALUES ('" + message.author.id + "', '" + resultmore5 + "' )")
+            print ("---4---")
+            print ('----MYSQL COMMAND START----')
+            print ("INSERT INTO dismona.id(id, address) VALUES ('" + message.author.id + "', '" + resultmore5 + "' )")
+            print ('----MYSQL COMMAND END----')
+            print ("---5---")
+            m = "<@"+ message.author.id + "> ,Created your account succefully! your address is " + resultmore5 + " enjoy!"
+            print ("---6---")
+            await client.send_message(message.channel, m)
 
     if message.content.startswith("/balance"):
         # 送り主がBotだった場合反応したくないので
