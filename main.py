930450ca01d1c5ab6e1fb75650de9e1f12e3b6cd
raspi0124 <adores0124@yahoo.co.jp>
2018-02-18 09:21:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name='Say /help'))
+    await client.change_presence(game=discord.Game(name='/help'))
 
 #message.author.name がユーザー名
 
