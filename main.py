0eff9be0389a60d6fdc159251ab993fb0b5c5994
raspi0124 <adores0124@yahoo.co.jp>
2018-02-18 22:49:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ async def on_ready():
     print(client.user.id)
     print('------')
     await client.change_presence(game=discord.Game(name='/help'))
-    await client.send_message(discord.Object(id='414478675132547073'), '```monage started. \n current time:' + currenttime + '')
+    await client.send_message(discord.Object(id='414478675132547073'), '```monage started. \n current time:' + currenttime + '```')
+    await client.send_message(discord.Object(id='414086589434822667'), '```monage started. \n current time:' + currenttime + '```')
+    
 
 #message.author.name がユーザー名
 
