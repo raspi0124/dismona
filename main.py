140b1acd55ea4b3cde9baf08353009982c8bb78d
raspi0124 <adores0124@yahoo.co.jp>
2018-03-01 22:59:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -270,7 +270,8 @@ async def on_message(message):
             sum = str(sum)
             m = "you will rain " + sum + "mona to " + raininfo[0] + " people."
             await client.send_message(message.channel, m)
-            sum = int(sum)
+            sum = str(sum)
+            #sum=amount to rain for each person
             cmd = "monacoin-cli listaccounts"
             rut  =  subprocess.check_output( cmd.split(" ") )
             data = rut.decode()
