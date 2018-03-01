770f9fedd6148449c25d1d0b69d682ced00b2ccb
raspi0124 <adores0124@yahoo.co.jp>
2018-03-01 15:44:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -267,7 +267,6 @@ async def on_message(message):
             print(raininfo[1])
             sum = float(raininfo[1]) / float(raininfo[0])
             print(sum)
-            sum = int(sum)
             m = "you will rain " + sum + "mona to " + raininfo[0] + " people."
             await client.send_message(message.channel, m)
             cmd = "monacoin-cli listaccounts"
