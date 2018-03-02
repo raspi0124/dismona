ef26ac6ff75e009b506eccfe92a39c770f5dedf8
raspi0124 <adores0124@yahoo.co.jp>
2018-03-03 00:56:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -306,18 +306,21 @@ async def on_message(message):
                 print(data[tosend])
                 numbertosend = int(numbertosend)
                 print ("--loop start--")
-                tosend = "1"
-                while tosend > "1":
-                    for var in range(0, numbertosend):
-                        tosend = random.randrange(1, 50, 1)
-                        print("--rondomfinish--")
-                        tosend = data[tosend]
-                        print("--startcommand--")
+                for var in range(0, numbertosend):
+                    tosend = random.randrange(1, 50, 1)
+                    print("--rondomfinish--")
+                    tosend = data[tosend]
+                    print("--startcommand--")
+
+                    if tosend <= "1":
                         cmd = "monacoin-cli move " + message.author.id + " " + tosend + " " + sum + ""
                         rut  =  subprocess.check_output( cmd.split(" ") )
                         print(rut)
                         m = "raining to <@" + tosend + ">.."
                         await client.send_message(message.channel, m)
+                    else:
+                        pass
+
                 m = "finished rain to " + raininfo[0] + "people! total amount was " + raininfo[1] + "mona!"
                 await client.send_message(message.channel, m)
                 print(rut)
