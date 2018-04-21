680f45a8417a2bbc51037aecb64b35e2efdc7d59
raspi0124 <adores0124@yahoo.co.jp>
2018-04-21 20:18:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -835,6 +835,6 @@ async def on_message(message):
 		```"
 		await client.send_message(message.channel, m)
 client.run("NDA5MDkwMTE4OTU2MDg5MzQ0.DbzaFA.hPWfWE9cXQc5UjsUbo17diRoBOQ")
-connection.close()
+cursor.close()
 # https://qiita.com/PinappleHunter/items/af4ccdbb04727437477f
 # https://qiita.com/komeiy/items/d6b5f25bf1778fa10e21
