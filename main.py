71595f32ca4f87c880c889676540a72a6ab67133
raspi0124 <adores0124@yahoo.co.jp>
2018-05-01 19:59:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from decimal import (Decimal, ROUND_DOWN)
 #import apim
 import sqlite3
 from datetime import datetime
-import monaparty
 
 def round_down5(value):
 	value = Decimal(value).quantize(Decimal('0.00001'), rounding=ROUND_DOWN)
@@ -911,6 +910,37 @@ async def on_message(message):
 			\n ---------------------------------------------------------------------------------- \
 			```"
 			await client.send_message(message.channel, m)
+
+
+
+			#MONAPARTY関連スタート
+			if message.content.startswith('/mp info'):
+				await client.add_reaction(message, '👌')
+			    headers = {
+			        'Content-Type': 'application/json; charset=UTF-8',
+			        'Accept': 'application/json, text/javascript',
+			    }
+			    data = '{ "jsonrpc": "2.0", "id": 0, "method": "get_running_info" }'
+			    response = requests.post('https://api.monaparty.me/api/counterparty', headers=headers, data=data, auth=('rpc', 'hello'))
+			    m = str(response)
+			    await client.send_message(reaction.message.channel, m)
+
+			if message.content.startswith('/mp balance'):
+			    address = re.split('\W+', message.content)
+			    addresses = address[2]
+			    addresses = '"' + addresses + '"'
+
+			    headers = {
+			        'Content-Type': 'application/json; charset=UTF-8',
+			        'Accept': 'application/json, text/javascript',
+			    }
+			    data = '{ "jsonrpc": "2.0", "id": 0, "method": "get_normalized_balances" "addresses": ' + addresses +' }'
+			    response = requests.post('https://wallet.monaparty.me/_api', headers=headers, data=data, auth=('rpc', 'hello'))
+			    m = "here is " + addresses + " balance" + response + ""
+
+
+
+				#MONAPARTY関連終わり
 
 
 
