70b02d033e0b8bb2bb479393a6ad491680d33406
raspi0124 <adores0124@yahoo.co.jp>
2018-05-01 20:05:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -913,36 +913,36 @@ async def on_message(message):
 
 
 
-			#MONAPARTY関連スタート
-			if message.content.startswith('/mp info'):
-				print("1")
-				await client.add_reaction(message, '👌')
-				headers = {
-					'Content-Type': 'application/json; charset=UTF-8',
-					'Accept': 'application/json, text/javascript',
-				}
-				data = '{ "jsonrpc": "2.0", "id": 0, "method": "get_running_info" }'
-				response = requests.post('https://api.monaparty.me/api/counterparty', headers=headers, data=data, auth=('rpc', 'hello'))
-				m = str(response)
-				await client.send_message(reaction.message.channel, m)
+		#MONAPARTY関連スタート
+		if message.content.startswith('/mp info'):
+			print("1")
+			await client.add_reaction(message, '👌')
+			headers = {
+				'Content-Type': 'application/json; charset=UTF-8',
+				'Accept': 'application/json, text/javascript',
+			}
+			data = '{ "jsonrpc": "2.0", "id": 0, "method": "get_running_info" }'
+			response = requests.post('https://api.monaparty.me/api/counterparty', headers=headers, data=data, auth=('rpc', 'hello'))
+			m = str(response)
+			await client.send_message(reaction.message.channel, m)
 
-			if message.content.startswith('/mp balance'):
-				print("1")
-				address = re.split('\W+', message.content)
-				addresses = address[2]
-				addresses = '"' + addresses + '"'
+		if message.content.startswith('/mp balance'):
+			print("1")
+			address = re.split('\W+', message.content)
+			addresses = address[2]
+			addresses = '"' + addresses + '"'
 
-				headers = {
-					'Content-Type': 'application/json; charset=UTF-8',
-					'Accept': 'application/json, text/javascript',
-				}
-				data = '{ "jsonrpc": "2.0", "id": 0, "method": "get_normalized_balances" "addresses": ' + addresses +' }'
-				response = requests.post('https://wallet.monaparty.me/_api', headers=headers, data=data, auth=('rpc', 'hello'))
-				m = "here is " + addresses + " balance" + response + ""
+			headers = {
+				'Content-Type': 'application/json; charset=UTF-8',
+				'Accept': 'application/json, text/javascript',
+			}
+			data = '{ "jsonrpc": "2.0", "id": 0, "method": "get_normalized_balances" "addresses": ' + addresses +' }'
+			response = requests.post('https://wallet.monaparty.me/_api', headers=headers, data=data, auth=('rpc', 'hello'))
+			m = "here is " + addresses + " balance" + response + ""
 
 
 
-				#MONAPARTY関連終わり
+			#MONAPARTY関連終わり
 
 
 
