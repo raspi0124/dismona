87d060577279ca7d65584698c4061c49a5cec33f
raspi0124 <admin@raspi0124.me>
2018-05-08 20:49:03 +0000

    Update 'mlibs.py'

--- mlibs.py
+++ mlibs.py
@@ -63,6 +63,8 @@ def deposit(userid):
 	currenttime = (datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 	elapsed_time = time.time() - start
 	elapsed_time = str(elapsed_time)
+	address = list(address)
+	address = address[0]
 	return address
 def withdraw(userid, to, amount):
 	start = time.time()
