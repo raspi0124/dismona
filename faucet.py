428d901eae1c3e51852685aed1e04a78dbc8740d
raspi0124 <admin@raspi0124.me>
2018-06-21 23:19:32 +0000

    Update 'faucet.py'

--- faucet.py
+++ faucet.py
@@ -1,5 +1,12 @@
-
 #!/usr/bin/python3
+# coding: utf-8
+import sys
+import io
+
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 import discord
 import subprocess
 import re
