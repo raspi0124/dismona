f98489d00dd83ed977083e8c08304145ab53cdb1
raspi0124 <adores0124@yahoo.co.jp>
2018-07-20 14:49:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -942,8 +942,7 @@ async def on_message(message):
 				print(json.dumps(responseresult[num]))
 				m = json.dumps(responseresult[num])
 				await client.send_message(message.channel, m)
-				m2 = "'''" + m + "'''"
-				json_dict = json.loads(m2)
+				json_dict = json.loads(m)
 				print('Asset名：{}'.format(json_dict['asset']))
 				print('Asset量：{}'.format(json_dict['normalized_quantity']))
 			responseresult = str(responseresult)
