b0a21a878b3af6613d375bd8aa20167d20ff48bb
raspi0124 <adores0124@yahoo.co.jp>
2018-07-23 13:47:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -973,6 +973,8 @@ async def on_message(message):
 			tiptoaddress = mlibs.deposit(tipto)
 			tiptoaddress = '"' + tiptoaddress + '"'
 			tiptoken = '"' + tiptoken + '"'
+			pubkey = mlibs.getpubkey(addresses)
+			pubkey = '"' + publkey + '"'
 			#APIにアクセスし該当TXIDをもらってくる
 			headers = {
 				'Content-Type': 'application/json; charset=UTF-8',
@@ -985,26 +987,26 @@ async def on_message(message):
   			"id": 1\n \
 			"method":"proxy_to_counterpartyd"\n \
 			}'
-			
+
 			data = '{\n \
-			"params":{\n \
-				"method":"create_send",\n \
 				"params":{\n \
-					fee_per_kb:150, //1KBあたりの手数料(Satoshi),\n \
-					disable_utxo_locks:true, //安全のために残高をロックする機能を無効化するか\n \
-					encoding:"auto", //autoで固定することを強く推奨\n \
-					pubkey:String //sourceの公開鍵の16進数。初めて使うアドレスなら必須,\n \
-					source:' + addresses + ' //送信元アドレス\n \
-					destination:' + tiptoaddress + ', //送金先アドレス\n \
-					asset:' + tiptoken + ', //トークン名。BTCとかXCPとかPEPECASHなど。\n \
-					quantity:' + tipamount + ', //送信量\n \
-					memo:"Monage tip service.", //メモ\n \
-				}\n \
-			},\n \
-		"id":0,\n \
-		"jsonrpc":"2.0",\n \
-		"method":"proxy_to_counterpartyd"\n \
-	}'
+					"method":"create_send",\n \
+					"params":{\n \
+						fee_per_kb:150, //1KBあたりの手数料(Satoshi),\n \
+						disable_utxo_locks:true, //安全のために残高をロックする機能を無効化するか\n \
+						encoding:"auto", //autoで固定することを強く推奨\n \
+						pubkey:' + pubkey + ' //sourceの公開鍵の16進数。初めて使うアドレスなら必須,\n \
+						source:' + addresses + ' //送信元アドレス\n \
+						destination:' + tiptoaddress + ', //送金先アドレス\n \
+						asset:' + tiptoken + ', //トークン名。BTCとかXCPとかPEPECASHなど。\n \
+						quantity:' + tipamount + ', //送信量\n \
+						memo:"Monage tip service.", //メモ\n \
+					}\n \
+				},\n \
+			"id":0,\n \
+			"jsonrpc":"2.0",\n \
+			"method":"proxy_to_counterpartyd"\n \
+			}'
 
 			print(data)
 			response = requests.post('https://wallet.monaparty.me/_api', headers=headers, data=data, auth=('rpc', 'hello'))
