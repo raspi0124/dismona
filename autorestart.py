def24d0e60b6ffe35afb9bf004a2141f90d47deb
raspi0124 <adores0124@yahoo.co.jp>
2018-08-13 22:06:34 +0900

    Test commit. Autorestart

---
+++ autorestart.py
@@ -0,0 +1,12 @@
+import subprocess
+
+cmd = "pgrep -a python | grep '/root/dismona/faucet.py'"
+process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+stderr=subprocess.PIPE)
+my_pid, err = process.communicate()
+
+if len(my_pid.splitlines()) >0:
+   print("Running")
+   exit()
+else:
+  print("Not Running")
