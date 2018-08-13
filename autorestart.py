bcfd9bd3a6b269bf1e597b4fd25efafabef2ab9a
raspi0124 <adores0124@yahoo.co.jp>
2018-08-13 22:38:41 +0900

    Added logging function

--- autorestart.py
+++ autorestart.py
@@ -1,15 +1,32 @@
 import subprocess
+import time
 
+timestamp = str(time.time())
 cmd = "pgrep -a python | grep '/root/dismona/faucet.py'"
 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
 stderr=subprocess.PIPE)
 my_pid, err = process.communicate()
 
+path_w = '/var/log/dismona.log'
 if len(my_pid.splitlines()) >0:
-   print("Running")
-   exit()
+	print("Running")
+	s = '' + timestamp + ' Faucet module is running.\n'
+
+	with open(path_w, mode='w') as f:
+		f.write(s)
+
+	with open(path_w) as f:
+		print(f.read())
+	exit()
 else:
-  print("Not Running!")
-  print("Restarting faucet process..")
-  cmd = "startfaucet"
-  rut = subprocess.check_output( cmdlib.split(" ") )
+	print("Not Running!")
+	print("Restarting faucet process..")
+	s = '' + timestamp + ' ERROR: Faucet module not running! Restarting..\n'
+
+	with open(path_w, mode='w') as f:
+		f.write(s)
+
+	with open(path_w) as f:
+		print(f.read())
+	cmd = "startfaucet"
+	rut = subprocess.check_output( cmdlib.split(" ") )
