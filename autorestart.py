8de6534a76cb6c38dcfa09b9f87defec2b19ef99
raspi0124 <adores0124@yahoo.co.jp>
2018-08-13 22:14:07 +0900

    Added autorestarting process for faucet module

--- autorestart.py
+++ autorestart.py
@@ -1,6 +1,6 @@
 import subprocess
 
-cmd = "pgrep -a python | grep '/root/dismona/aaa.py'"
+cmd = "pgrep -a python | grep '/root/dismona/faucet.py'"
 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
 stderr=subprocess.PIPE)
 my_pid, err = process.communicate()
@@ -9,4 +9,7 @@ if len(my_pid.splitlines()) >0:
    print("Running")
    exit()
 else:
-  print("Not Running")
+  print("Not Running!")
+  print("Restarting faucet process..")
+  cmd = "startfaucet"
+  rut = subprocess.check_output( cmdlib.split(" ") )
