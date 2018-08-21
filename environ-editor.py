6a6e516aee6d280d214669fbc0962e876112897e
raspi0124 <adores0124@yahoo.co.jp>
2018-08-21 14:17:43 +0900

    trial fix

--- environ-editor.py
+++ environ-editor.py
@@ -6,7 +6,7 @@ action = input("Enter action [0 for adding, 1 for deleting, 2 for checking and 3
 if action == "0":
 	keyname = input("Please first enter keyname for enviroment: ")
 	keycontent = input("Now, Please enter key content for keyname above: ")
-	os.environ[keyname] = keycontent
+	os.environ["" + keyname + ""] = "" + keycontent + ""
 if action == "1":
 	keyname = input("Please enter keyname you want to delete: ")
 	sure = input("Are you sure you want to remove keyname " + keyname + "? If so, Please enter y or else, please enter n: ")
