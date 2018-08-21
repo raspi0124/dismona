df81b79dc6123373c511d3c320463fa22c24d961
raspi0124 <adores0124@yahoo.co.jp>
2018-08-21 15:15:23 +0900

    test commit

--- environ-editor.py
+++ environ-editor.py
@@ -2,11 +2,12 @@ import os
 import sys
 #TESTACTION
 os.environ["TEST"] = "test2"
+
 action = input("Enter action [0 for adding, 1 for deleting, 2 for checking and 3 for debuging]: ")
 if action == "0":
 	keyname = input("Please first enter keyname for enviroment: ")
 	keycontent = input("Now, Please enter key content for keyname above: ")
-	os.environ["" + keyname + ""] = "" + keycontent + ""
+	os.environ["keyname"] = "keycontent"
 if action == "1":
 	keyname = input("Please enter keyname you want to delete: ")
 	sure = input("Are you sure you want to remove keyname " + keyname + "? If so, Please enter y or else, please enter n: ")
