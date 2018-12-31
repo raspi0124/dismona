52abf4c6606642c49edc9cd13a507ecb0825a076
raspi0124 <adores0124@yahoo.co.jp>
2019-01-01 01:12:39 +0900

    Decreased amount for otoshidama

--- faucet.py
+++ faucet.py
@@ -814,7 +814,7 @@ async def on_message(message):
 			if userid not in given_otoshidama:
 				balance = mlibs.libgetbalance(userid)
 				#残高の20分の1
-				giving = float(balance) * float("0.05")
+				giving = float(balance) * float("0.02")
 				minimum = float("0.00010")
 				maximum = float("0.2")
 				giving = round_down5(giving)
